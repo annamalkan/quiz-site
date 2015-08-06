2b0a44b1c460dc2dfd3b37e204a0eb3d410ae51a
annamalkan <annamalkan@gmail.com>
2015-08-06 19:01:51 +0200

    skapade Question model

--- quiz/models.py
+++ quiz/models.py
@@ -6,3 +6,12 @@ class Quiz(models.Model):
 	slug = models.SlugField(max_length=100)
 	description = models.TextField()
 
+class Question(models.Model):
+	quiz = models.ForeignKey(Quiz, related_name="questions")
+	question = models.TextField()
+	answer1 = models.CharField(max_length=100)
+	answer2 = models.CharField(max_length=100)
+	answer3 = models.CharField(max_length=100)
+	correct = models.PositiveIntegerField()
+
+	

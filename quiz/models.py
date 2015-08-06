acc2de05a59db1b02c016b7d272c3272038a6365
annamalkan <annamalkan@gmail.com>
2015-08-06 18:57:43 +0200

    skapade Quiz model

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Quiz(models.Model):
+	name = models.CharField(max_length=100)
+	slug = models.SlugField(max_length=100)
+	description = models.TextField()
+

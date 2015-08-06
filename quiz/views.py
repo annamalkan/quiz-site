4a32a5a16da718190b338e0f345e1ee144ac262f
annamalkan <annamalkan@gmail.com>
2015-08-06 11:34:33 +0200

    changed text to my text rather than Emil's example text

--- quiz/views.py
+++ quiz/views.py
@@ -3,15 +3,15 @@
 from django.shortcuts import render
 quizzes = {
 	"klassiker": {
-   		"name": u"Klassiska böcker",
+   		"name": u"Political heroes",
 	   	"description": u"Hur bra kan du dina klassiker?"
 	},
 	"fotboll": {
-	   	"name": u"Största fotbollslagen",
-	   	"description": u"Kan du dina lag?"
+	   	"name": u"Sports stars",
+	   	"description": u"How many of these spectacular sportswomen are you familiar with? Find out and learn more about all of them by completing this quiz!"
 	},
 	"kanda-hackare": {
-	    	"name": u"Världens mest kända hackare",
+	    	"name": u"Scientific masterminds",
 	    	"description": u"Hackerhistoria är viktigt, kan du den?"	},
 }
 

b63a487b5895dbb0a9f36608d63245a734eb4a21
annamalkan <annamalkan@gmail.com>
2015-08-07 08:55:37 +0200

    registered and imported models

--- quiz/models.py
+++ quiz/models.py
@@ -5,6 +5,8 @@ class Quiz(models.Model):
 	name = models.CharField(max_length=100)
 	slug = models.SlugField(max_length=100)
 	description = models.TextField()
+	def __unicode__(self):
+		return self.name
 
 class Question(models.Model):
 	quiz = models.ForeignKey(Quiz, related_name="questions")
@@ -13,5 +15,6 @@ class Question(models.Model):
 	answer2 = models.CharField(max_length=100)
 	answer3 = models.CharField(max_length=100)
 	correct = models.PositiveIntegerField()
+	def __unicode__(self):
+		return self.quiz.name + "/ " + self.question
 
-	

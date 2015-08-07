32bcc00c49eef9da27268a068e3012d02cb39d85
annamalkan <annamalkan@gmail.com>
2015-08-07 15:01:31 +0200

    ..

--- quizsite/settings.py
+++ quizsite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
+import dj_database_url
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -74,7 +74,8 @@ WSGI_APPLICATION = 'quizsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-import dj_database_url
+
+
 DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:////{0}'.format(os.path.join(BASE_DIR,'db.sqlite3'))

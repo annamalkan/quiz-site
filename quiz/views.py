86f8d62df9e87ecb07ce4fe7cf0ad5d50b38d19c
annamalkan <annamalkan@gmail.com>
2015-08-07 11:20:00 +0200

    Go to completed page if questions are done

--- quiz/views.py
+++ quiz/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render
 from quiz.models import Quiz
+from django.shortcuts import redirect
 
 # Create your views here.
 
@@ -20,6 +21,8 @@ def question(request, slug, number):
 	number = int(number)
 	quiz = Quiz.objects.get(slug=slug)
 	questions = quiz.questions.all()
+	if number > questions.count():
+		return redirect("completed_page", quiz.slug)
 	question = questions[number - 1]
 	image = question.image
 	context = {
